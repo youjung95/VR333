42ad1f933109be61c32c85d3931b63e464d902bb
youjung95 <youjung94111@gmail.com>
2024-10-18 15:47:33 +0900

    1

---
+++ data.js
@@ -0,0 +1,502 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "전체뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4119325503542375
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4105716823771779,
+          "pitch": -0.45972238509510177,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.512390048928518,
+          "pitch": -0.4420095247308211,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.6115131835664034,
+          "pitch": -0.4183302921578438,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.6974225047351794,
+          "pitch": -0.3956563661118899,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.776532492605071,
+          "pitch": -0.3695320653602199,
+          "rotation": 0,
+          "target": "4-1"
+        },
+        {
+          "yaw": 0.8432917131156721,
+          "pitch": -0.3430583960999183,
+          "rotation": 0,
+          "target": "6-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.45737658725963826,
+          "pitch": -0.2895305335961389,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀</font></font></font></font>",
+          "text": "<div>바로크 로코코시대 양식을 따온 현대식 웨딩홀로 건물 2층의 몰딩 모양은 티아라를 모티브하여 건물의&nbsp;</div><div>상징성을 주었으며, 기둥은 흰색 방수페인트 마감을 사용하였으며 벽은 하늘색 스타코마감을 하고, 금장 티타늄 몰딩을 더해 웅장하고 아름다운 건물을 만들었습니다.</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "조경뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4841280884111825
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.505086978440108,
+          "pitch": -0.48320379971615957,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.8908962093175354,
+          "pitch": -0.13871446471212678,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.8917735518798278,
+          "pitch": -0.061285439429186184,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.8917727525533721,
+          "pitch": 0.016733472832257235,
+          "rotation": 0,
+          "target": "4-1"
+        },
+        {
+          "yaw": -0.8908959737104425,
+          "pitch": 0.09603513635632765,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": 0.10618729278670536,
+          "pitch": 0.04633629119994964,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7175190105663418,
+          "pitch": 0.17552058333967224,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">분수대 조경</font></font></font></font>",
+          "text": "<div>궁전조경을 모티브하여 표현하였습니다. 작게나마 분수대와 벤치를 활용하였고 영원한 사랑의 꽃말인 이팝나무와 분홍 병꽃나무로 조경의 화사함을 더했였습니다.</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "입구뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.667569557302361,
+          "pitch": -0.5429036671920748,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.9762400425379703,
+          "pitch": -0.21551528411942478,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.976240304358118,
+          "pitch": -0.15059904424520454,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.9755422830511034,
+          "pitch": -0.08199368185330158,
+          "rotation": 0,
+          "target": "4-1"
+        },
+        {
+          "yaw": -0.9762397059117696,
+          "pitch": -0.011194073098193869,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": 0.930920094308739,
+          "pitch": 0.12399503657741207,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4326924426503158,
+          "pitch": 0.34987157346461295,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀 입구</font></font></font></font>",
+          "text": "<div>웨딩홀의 입구벽은 흰색 몰딩과 하늘색 스타고 벽마감 아치형 창을 활용하였으며 아름다움을 더여하였으며 티아라 조형물로 포인트를 주고,&nbsp;</div><div>입구에 신성한 느낌을 주는 조형물을 배치하였습니다.</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "주례뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9912256593126436,
+          "pitch": 0.18278868383771218,
+          "rotation": 4.71238898038469,
+          "target": "4-1"
+        },
+        {
+          "yaw": 0.9831361364316518,
+          "pitch": 0.18499080233878828,
+          "rotation": 1.5707963267948966,
+          "target": "6-2"
+        },
+        {
+          "yaw": -0.024421510895974663,
+          "pitch": 0.6836452522483327,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.4739578221494547,
+          "pitch": -0.245850266322174,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.4999697082814052,
+          "pitch": -0.24628795653709723,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.06872945623049809,
+          "pitch": -0.6932118158345304,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4686713651161085,
+          "pitch": 0.1006939072548576,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">주례석</font></font>",
+          "text": "<div>신랑 신부가 모두가 보는 앞에서 결혼의 약속을 맺는 신성한 위치로 신성하고 성스러운 조형물과 작은 분수대를 배치였으며,금색 몰딩과 영원한 사랑의 상징인 이팝나무를 배치하여 더 많은 화사함과 아름다운 느낌을 표현하였습니다.</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-1",
+      "name": "하객1뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0061946290704533,
+          "pitch": 0.04156250839036346,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.0061946290704533,
+          "pitch": 0.10535665720444598,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.005559261873028,
+          "pitch": 0.246999013514273,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.0080935519012328,
+          "pitch": -0.02960730017879243,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": -1.0055586462850243,
+          "pitch": 0.17424120829092438,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">하객뷰</font></font>",
+          "text": "<div>사각 커튼월을 사용하여 개방감을 주고 디자인적으로 아름다움을 주었으며 흰색 페인트벽과 금색 몰딩을&nbsp;</div><div>사용하여 화사함을 더하여 표현했습니다</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "버진로드뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7287482838475459,
+          "pitch": -0.5653757610804941,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.6620684890673445,
+          "pitch": -0.5908541463278567,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.8172760532533108,
+          "pitch": 0.25418742569267394,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.8445229903104661,
+          "pitch": 0.2704433796518444,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-1"
+        },
+        {
+          "yaw": -0.579932162743523,
+          "pitch": -0.613403584855913,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.14552225015736298,
+          "pitch": 0.3766028690560077,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">버진로드</font></font>",
+          "text": "<div>하늘색의 광택 대리석으로 버진로드 길을 제작하여 화사한 느낌을 주었으며 양측에 흰색 꽃을 활용하였고</div><div>금색 몰딩과 모든 벽이 커튼월로 제작해 개방감을 주어 야외에서 결혼하는 느낌을 느낄수 있도록 설계했습니다.</div>"
+        }
+      ]
+    },
+    {
+      "id": "6-2",
+      "name": "하객2뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3954747685346369,
+          "pitch": 0.0838195457087565,
+          "rotation": 0,
+          "target": "4-1"
+        },
+        {
+          "yaw": -0.7564653889925363,
+          "pitch": 0.36272175767283343,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.0168218159310545,
+          "pitch": 0.266835812001327,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.5845792279162048,
+          "pitch": -0.14526462531274298,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.49825751081510994,
+          "pitch": -0.1490812317127812,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.39547464163853974,
+          "pitch": -0.1584952952650589,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.245847579956898,
+          "pitch": -0.16434731456862117,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">하객뷰</font></font>",
+          "text": "<div>사각 커튼월을 사용하여 개방감을 주고 디자인적으로 아름다움을 주었으며 흰색 페인트벽과 금색 몰딩을&nbsp;</div><div>사용하여 화사함을 더하여 표현했습니다&nbsp;&nbsp;</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
